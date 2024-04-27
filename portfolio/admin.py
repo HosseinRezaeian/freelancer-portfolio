2b19885d65d71793d7adde94d4a9bd7a99062b88
hosseinrezaeian <hossainrezaeian@gmail.com>
2024-04-27 21:17:55 +0330

    add permions for add just one record in a table

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from .models import Profile,category_gallery,picture_gallery,About,Skills,Services,EducationAndExpericence
+from .models import Profile, category_gallery, picture_gallery, About, Skills, Services, EducationAndExpericence
+
+MAX_OBJECTS = 1
 
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        if self.model.objects.count() >= MAX_OBJECTS:
+            return False
+        return super().has_add_permission(request)
+
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(About)
 admin.site.register(Skills)
 admin.site.register(Services)
 admin.site.register(EducationAndExpericence)
 admin.site.register(category_gallery)
 admin.site.register(picture_gallery)
-# Register your models here.
